3e5dee7921eb49ca55bb79917ac1719bffef66c2
Nikhil Joglekar <nikhiljoglekar@Utexas.edu>
2014-09-07 22:11:54 -0500

    init commit

---
+++ Lab1/Lab1.c
@@ -0,0 +1,10 @@
+#include <stdio.h> /* standard input/output library */
+#include <stdlib.h> /* Standard C Library */
+#include <string.h> /* String operations library */
+#include <ctype.h> /* Library for useful character operations */
+#include <limits.h> /* Library for definitions of common variable type characteristics */
+
+int main() {
+	printf("hello\n");
+	getchar();
+}
