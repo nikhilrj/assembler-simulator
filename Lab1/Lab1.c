4b2fae65a3b5adc230ac8262097255daa17ee206
Nikhil Joglekar <nikhiljoglekar@Utexas.edu>
2014-09-10 00:31:59 -0500

    linking up started code

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -5,81 +5,88 @@
 #include <limits.h> /* Library for definitions of common variable type characteristics */
 
 #define MAX_LINE_LENGTH 255
+
+const char* opCodeList[] = { "ADD", "AND", "BR", "HALT", "JMP", "JSR", "JSRR", "LDB", "LDW", "LEA", "NOP", "NOT", "RET", "LSHF", "RSHFL", "RSHFA", "RTI", "STB", "STW", "TRAP", "XOR" };
+
 enum
 {
 	DONE, OK, EMPTY_LINE
 };
 
-bool isOpCode(char* opCode)
+int isOpcode(char* opCode)
 {
-	char* opCodeList[] = {"ADD","AND","BR","HALT","JMP","JSR","JSRR","LDB","LDW","LEA","NOP","NOT","RET","LSHF","RSHFL","RSHFA","RTI","STB","STW","TRAP","XOR"};
+	return 1;
+	/*TODO: finish function*/
 	int i;
-	for(i = 0; i<sizeof(opCodeList)
+	for (i = 0; i < sizeof(opCodeList) / sizeof(char*); i++)
+	{
+
+	}
 }
 
-int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4)
+int readAndParse(FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4)
 {
-	char * lRet, * lPtr;
+	char * lRet, *lPtr;
 	int i;
-	if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
-		return( DONE );
-	for( i = 0; i < strlen( pLine ); i++ )
-		pLine[i] = tolower( pLine[i] );
+	if (!fgets(pLine, MAX_LINE_LENGTH, pInfile))
+		return(DONE);
+	for (i = 0; i < strlen(pLine); i++)
+		pLine[i] = tolower(pLine[i]);
 
 	/* convert entire line to lowercase */
 	*pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
 	/* ignore the comments */
 	lPtr = pLine;
-	while( *lPtr != ';' && *lPtr != '\0' &&
-		*lPtr != '\n' ) 
+	while (*lPtr != ';' && *lPtr != '\0' &&
+		*lPtr != '\n')
 		lPtr++;
 	*lPtr = '\0';
-	if( !(lPtr = strtok( pLine, "\t\n ," ) ) ) 
-		return( EMPTY_LINE );
-	if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+	if (!(lPtr = strtok(pLine, "\t\n ,")))
+		return(EMPTY_LINE);
+	if (isOpcode(lPtr) == -1 && lPtr[0] != '.') /* found a label */
 	{
 		*pLabel = lPtr;
-		if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+		if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
 	}
 
 	*pOpcode = lPtr;
-	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
 
 	*pArg1 = lPtr;
 
-	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
 	*pArg2 = lPtr;
-	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
 	*pArg3 = lPtr;
-	if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+	if (!(lPtr = strtok(NULL, "\t\n ,"))) return(OK);
 	*pArg4 = lPtr;
-	return( OK );
+	return(OK);
 }
 
-int toNum( char * pStr )
+int toNum(char * pStr)
 {
 	char * t_ptr;
 	char * orig_pStr;
-	int t_length,k;
+	int t_length, k;
 	int lNum, lNeg = 0;
 	long int lNumLong;
 
 	orig_pStr = pStr;
-	if( *pStr == '#' )				/* decimal */
-	{ 
+	if (*pStr == '#')				/* decimal */
+	{
 		pStr++;
-		if( *pStr == '-' )				/* dec is negative */
+		if (*pStr == '-')				/* dec is negative */
 		{
 			lNeg = 1;
 			pStr++;
 		}
 		t_ptr = pStr;
 		t_length = strlen(t_ptr);
-		for(k=0;k < t_length;k++)
+		for (k = 0; k < t_length; k++)
 		{
 			if (!isdigit(*t_ptr))
 			{
-				printf("Error: invalid decimal operand, %s\n",orig_pStr);
+				printf("Error: invalid decimal operand, %s\n", orig_pStr);
 				exit(4);
 			}
 			t_ptr++;
@@ -90,39 +97,63 @@ int toNum( char * pStr )
 
 		return lNum;
 	}
-	else if( *pStr == 'x' )	/* hex     */
+	else if (*pStr == 'x')	/* hex     */
 	{
 		pStr++;
-		if( *pStr == '-' )				/* hex is negative */
+		if (*pStr == '-')				/* hex is negative */
 		{
 			lNeg = 1;
 			pStr++;
 		}
 		t_ptr = pStr;
 		t_length = strlen(t_ptr);
-		for(k=0;k < t_length;k++)
+		for (k = 0; k < t_length; k++)
 		{
 			if (!isxdigit(*t_ptr))
 			{
-				printf("Error: invalid hex operand, %s\n",orig_pStr);
+				printf("Error: invalid hex operand, %s\n", orig_pStr);
 				exit(4);
 			}
 			t_ptr++;
 		}
 		lNumLong = strtol(pStr, NULL, 16);    /* convert hex string into integer */
-		lNum = (lNumLong > INT_MAX)? INT_MAX : lNumLong;
-		if( lNeg )
+		lNum = (lNumLong > INT_MAX) ? INT_MAX : lNumLong;
+		if (lNeg)
 			lNum = -lNum;
 		return lNum;
 	}
 	else
 	{
-		printf( "Error: invalid operand, %s\n", orig_pStr);
+		printf("Error: invalid operand, %s\n", orig_pStr);
 		exit(4);  /* This has been changed from error code 3 to error code 4, see clarification 12 */
 	}
 }
 
-int main() {
-	printf("hello\n");
+int main(int argc, char* argv[]) {
+	char *prgName = NULL;
+	char *iFileName = NULL;
+	char *oFileName = NULL;
+
+	prgName = argv[0];
+	iFileName = argv[1];
+	oFileName = argv[2];
+
+	char lLine[MAX_LINE_LENGTH + 1], *lLabel, *lOpcode, *lArg1,	*lArg2, *lArg3, *lArg4;
+
+	int lRet;
+
+	FILE * lInfile;
+
+	lInfile = fopen(iFileName, "r");	/* open the input file */
+
+	do
+	{
+		lRet = readAndParse(lInfile, lLine, &lLabel, &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4);
+		if (lRet != DONE && lRet != EMPTY_LINE)
+		{
+			printf("%d\n", lRet);
+		}
+	} while (lRet != DONE);
+
 	getchar();
 }
